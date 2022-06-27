a32bf61e136aa2ce4dff1f8a8662cc5974a8c14f
Matt Scalzo <matthewfscalzo@gmail.com>
2022-06-27 15:59:05 -0400

    Implement marry.py

---
+++ marry.py
@@ -0,0 +1,41 @@
+# first letter in s1, going forwards is it in s2?
+# remove chars until it is
+
+
+def canMarry(s1, s2):
+    if (s1 in s2 or s2 in s1):
+        return "YES"
+
+    string1 = s1
+    string2 = s2
+
+    for x in range(len(s1)):
+        while s1[x] != string2[x]:
+            string2 = string2[0 : x :] + string2[x + 1 : :]
+            
+            if len(string2) <= x:
+                return "NO"
+
+    if (s1 in string2 or string2 in s1):
+        return "YES"
+
+    for x in range(len(s2)):
+        while string1[x] != s2[x]:
+            string1 = string1[0 : x :] + string1[x + 1 : :]
+
+            if len(string1) <= x:
+                return "NO"
+
+    if (string1 in s2 or s2 in string1):
+        return "YES"
+
+    return "NO"
+
+T = int(input())
+A = []
+for i in range(T):
+    A.append(input())
+
+for x in A:
+    names = x.split()
+    print(canMarry(names[0], names[1]))
